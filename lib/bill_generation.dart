11c177140d6a1a421b3b959b02fccbfea3b08469
Rakshana <rakshana.jp@gmail.com>
2020-03-27 05:43:36 +0530

    basic ui for bill generation

--- lib/bill_generation.dart
+++ lib/bill_generation.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class BillGeneration extends StatefulWidget {
@@ -6,6 +7,15 @@ class BillGeneration extends StatefulWidget {
 }
 
 class _BillGenerationState extends State<BillGeneration> {
+
+  final Firestore _firestore = Firestore.instance;
+  String house;
+  int currentReading;
+
+  void _generateBill(String house, int currentReading) {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,15 +27,50 @@ class _BillGenerationState extends State<BillGeneration> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+        child: ListView(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(30, 20, 30, 0),
+              child: TextField(
+                onChanged: (value) {
+                  house = value;
+                },
+                decoration: InputDecoration(
+                  labelText: 'House number',
+                  border: new OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.fromLTRB(30, 20, 30, 0),
+              child: TextField(
+                onChanged: (value) {
+                  currentReading = int.parse(value);
+                },
+                decoration: InputDecoration(
+                  labelText: 'Current Reading',
+                  border: new OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            RaisedButton(
+              onPressed: () {
+                _generateBill(house, currentReading);
+              },
+              child: Text('Generate bill'),
+            ),
+
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
