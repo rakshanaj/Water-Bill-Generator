f97aa345c73bdf3112068e06115539d13556b3d1
Rakshana <rakshana.jp@gmail.com>
2020-04-02 15:10:06 +0530

    added month names

--- lib/previous_list.dart
+++ lib/previous_list.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -10,120 +8,10 @@ class PreviousList extends StatefulWidget {
 
 class _PreviousListState extends State<PreviousList> {
 
-  @override
-  void initState() {
-    super.initState();
-    y = 0;
-  }
-
   int selectedYear;
 
   final Firestore _firestore = Firestore.instance;
-  String x;
-  int y;
 
-  Future<String> _getValue(String house, String month, String year) async {
-    await _firestore
-        .collection('readings')
-        .document(year)
-        .collection(month)
-        .getDocuments()
-        .then((QuerySnapshot snapshot) {
-      snapshot.documents.forEach((f) {
-        if (f.data['house'] == house) {
-          x = f.data['bill'].toString();
-          return x;
-        }
-        return 'no';
-      });
-    });
-  }
-
-  List<Widget> _buildCells(int count, bool isHouse) {
-    return List.generate(
-      count,
-          (index) =>
-          Container(
-            alignment: Alignment.center,
-            width: 120.0,
-            height: 60.0,
-            color: Colors.white,
-            margin: EdgeInsets.all(4.0),
-            child: isHouse
-                ? index == 0
-                ? Text(
-              'House number',
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-                //fontStyle: FontStyle.italic,
-              ),
-            )
-                : index < 3
-                ? Text('f$index')
-                : index > 5 ? Text('t${index - 5}') : Text('s${index - 2}')
-                : Text('bruh what'),
-            //: Text('${_getValue('f1', '3', '2020')}'),
-          ),
-    );
-  }
-
-  Widget _buildData(String month, String year, String house) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: _firestore
-          .collection('readings')
-          .document(year)
-          .collection(month)
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final houses = snapshot.data.documents;
-          List<MonthReads> monthlyreadings = [];
-          print('y is $y');
-          if (y == 0) {
-            monthlyreadings.add(MonthReads(
-              house: house,
-              month: month,
-              year: year,
-              current: int.parse(month),
-            ));
-            y++;
-          }
-          for (var h in houses) {
-            if (h.data['house'] == house) {
-              final fbHouse = h.data['house'];
-              final fbCurrent = h.data['current'];
-
-              final box = MonthReads(
-                month: month,
-                year: year,
-                house: fbHouse,
-                current: fbCurrent,
-              );
-
-              monthlyreadings.add(box);
-            }
-//            else if(house == 's2'){
-//              final box = MonthReads(
-//                month: month,
-//                year: year,
-//                house: house,
-//                current: 0,
-//              );
-//
-//              monthlyreadings.add(box);
-//            }
-          }
-
-          return Column(
-
-            children: monthlyreadings,
-          );
-        }
-        return Text('not yet bro');
-      },
-    );
-  }
 
   Widget _buildBody(BuildContext context) {
     int year = 2020,
@@ -137,14 +25,44 @@ class _PreviousListState extends State<PreviousList> {
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
         return DataTable(
-          columns: [
-            DataColumn(label: Text('House Number')),
-            DataColumn(label: Text('month $month')),
-          ],
+          columns: _columnList(),
           rows: _buildList(context, snapshot.data.documents),
         );
       },
     );
+  }
+
+
+  List<DataColumn> _columnList() {
+    List months = [
+      'Jan',
+      'Feb',
+      'MArch',
+      'April',
+      'may',
+      'June',
+      'July',
+      'Aug',
+      'Sept',
+      'Oct',
+      'November',
+      'December'
+    ];
+
+    List<DataColumn> list = [];
+    for (var i = 0; i < 2; i++) {
+      if (i == 0) {
+        list.add(DataColumn(
+            label: Text('house')
+        ));
+      }
+      else {
+        list.add(DataColumn(
+          label: Text(months[i + 1]),
+        ));
+      }
+    }
+    return list;
   }
 
   List<DataRow> _buildList(BuildContext context,
