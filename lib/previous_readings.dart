425dce706fc1dfb546b24d6f4875eac328bb9796
Rakshana <rakshana.jp@gmail.com>
2020-04-12 01:41:07 +0530

    ui done :'((((((( for the 10th timeee

--- lib/previous_readings.dart
+++ lib/previous_readings.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:water_bill_manager/house_listview.dart';
 
 class PreviousReadings extends StatefulWidget {
   @override
@@ -20,6 +19,135 @@ class _PreviousReadingsState extends State<PreviousReadings>
     super.initState();
   }
 
+  Widget _boxes(String img, String text1, String text2, Color color1,
+      Color color2) {
+    return SizedBox(
+      //width of entire column basically
+      width: 130,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 32, left: 8, right: 8, bottom: 8),
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                      color: Colors.blueGrey.withOpacity(0.6),
+                      offset: const Offset(1.1, 4.0),
+                      blurRadius: 8.0),
+                ],
+                gradient: LinearGradient(
+                  colors: [
+                    color1, color2
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: const BorderRadius.only(
+                  bottomRight: Radius.circular(8.0),
+                  bottomLeft: Radius.circular(8.0),
+                  topLeft: Radius.circular(8.0),
+                  topRight: Radius.circular(54.0),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 54, left: 16, right: 8, bottom: 8),
+                child: Container(
+                  height: 80,
+                  width: 400,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        text1,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          letterSpacing: 0.2,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding:
+                          const EdgeInsets.only(top: 8, bottom: 8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment:
+                            CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                text2,
+                                style: TextStyle(
+                                  //fontFamily: FintnessAppTheme.fontName,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 15,
+                                  letterSpacing: 0.2,
+                                  color: Colors.white.withOpacity(0.75),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(60, 0, 0, 0),
+                        decoration: BoxDecoration(
+                          color: Colors.white70,
+                          shape: BoxShape.circle,
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                                color: Colors.black12.withOpacity(0.4),
+                                offset: Offset(8.0, 8.0),
+                                blurRadius: 8.0),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: Icon(
+                            Icons.keyboard_arrow_right,
+                            color: Colors.blueGrey,
+                            size: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 8,
+            left: 8,
+            child: SizedBox(
+              width: 50,
+              height: 50,
+              child: Image.asset(img),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,148 +156,60 @@ class _PreviousReadingsState extends State<PreviousReadings>
         // the App.build method, and use it to set our appbar title.
         title: Text('Tenant Profile'),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.height / 2,
-            child: HouseListView(
-              mainScreenAnimation: Tween<double>(begin: 0.0, end: 1.0).animate(
-                  CurvedAnimation(
-                      parent: animationController,
-                      curve: Interval((1 / 9) * 3, 1.0,
-                          curve: Curves.fastOutSlowIn))),
-              mainScreenAnimationController: animationController,
+
+//          Container(
+//            height: MediaQuery.of(context).size.height * 0.05,
+//            child: HouseListView(
+//              mainScreenAnimation: Tween<double>(begin: 0.0, end: 1.0).animate(
+//                  CurvedAnimation(
+//                      parent: animationController,
+//                      curve: Interval((1 / 9) * 3, 1.0,
+//                          curve: Curves.fastOutSlowIn))),
+//              mainScreenAnimationController: animationController,
+//            ),
+//          ),
+      body: Container(
+        height: MediaQuery
+            .of(context)
+            .size
+            .height * 0.8,
+        margin: EdgeInsets.all(15),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                _boxes(
+                    'assets/images/f.png', 'F1', 'xyz', Colors.brown.shade700,
+                    Colors.brown.shade300),
+                _boxes('assets/images/f.png', 'F2', 'Roshan',
+                    Colors.brown.shade700, Colors.brown.shade300),
+              ],
             ),
-          ),
-          Container(
-            child: SizedBox(
-              width: 130,
-              child: Stack(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        top: 32, left: 8, right: 8, bottom: 16),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        boxShadow: <BoxShadow>[
-                          BoxShadow(
-                              color: Colors.blueGrey.withOpacity(0.6),
-                              offset: const Offset(1.1, 4.0),
-                              blurRadius: 8.0),
-                        ],
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.brown.shade700,
-                            Colors.brown.shade400
-                          ],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        ),
-                        borderRadius: const BorderRadius.only(
-                          bottomRight: Radius.circular(8.0),
-                          bottomLeft: Radius.circular(8.0),
-                          topLeft: Radius.circular(8.0),
-                          topRight: Radius.circular(54.0),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            top: 54, left: 16, right: 16, bottom: 8),
-                        child: Container(
-                          height: 100,
-                          width: 400,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Text(
-                                'shit',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  //fontFamily: FintnessAppTheme.fontName,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                  letterSpacing: 0.2,
-                                  color: Colors.white70,
-                                ),
-                              ),
-                              Expanded(
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.only(top: 8, bottom: 8),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Text(
-                                        'poo poo',
-                                        style: TextStyle(
-                                          //fontFamily: FintnessAppTheme.fontName,
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: 10,
-                                          letterSpacing: 0.2,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                margin: EdgeInsets.fromLTRB(50, 0, 0, 0),
-                                decoration: BoxDecoration(
-                                  color: Colors.white70,
-                                  shape: BoxShape.circle,
-                                  boxShadow: <BoxShadow>[
-                                    BoxShadow(
-                                        color: Colors.black12.withOpacity(0.4),
-                                        offset: Offset(8.0, 8.0),
-                                        blurRadius: 8.0),
-                                  ],
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4),
-                                  child: Icon(
-                                    Icons.add,
-                                    color: Colors.blueGrey,
-                                    size: 24,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 0,
-                    left: 0,
-                    child: Container(
-                      width: 84,
-                      height: 84,
-                      decoration: BoxDecoration(
-                        color: Colors.amber,
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 8,
-                    left: 15,
-                    child: SizedBox(
-                      width: 50,
-                      height: 50,
-                      child: Image.asset('assets/images/shit.png'),
-                    ),
-                  )
-                ],
-              ),
+            Row(
+              children: <Widget>[
+                _boxes('assets/images/s.png', 'S1', 'Gowtham',
+                    Colors.blue.shade900, Colors.lightBlueAccent.shade400),
+                _boxes(
+                    'assets/images/s.png', 'S2', 'Shane', Colors.blue.shade900,
+                    Colors.lightBlueAccent.shade400),
+                _boxes('assets/images/s.png', 'S3', 'Sathish',
+                    Colors.blue.shade900, Colors.lightBlueAccent.shade400),
+              ],
             ),
-          ),
-        ],
+            Row(
+              children: <Widget>[
+                _boxes(
+                    'assets/images/t.png', 'T1', 'Ankit', Colors.red.shade900,
+                    Colors.red.shade200),
+                _boxes(
+                    'assets/images/t.png', 'T1', 'Shastri', Colors.red.shade900,
+                    Colors.red.shade200),
+                _boxes('assets/images/t.png', 'T1', 'boys', Colors.red.shade900,
+                    Colors.red.shade200),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
